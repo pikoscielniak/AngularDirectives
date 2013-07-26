680fea18d9d5a482aaaea8400e70f006c1a76b15
piotrekbass <piotrekbass@interia.pl>
2013-07-26 22:35:31 +0200

    using require in directive

--- public/js/directives/greeting.js
+++ public/js/directives/greeting.js
@@ -11,9 +11,38 @@ app.directive('greeting', function () {
     };
 });
 
+app.directive('finnish', function () {
+    "use strict";
+
+    return {
+        require: 'greeting',
+        restrict: 'A',
+        link: function (scope, element, attrs, controller) {
+            controller.addGreeting('hei');
+        }
+    };
+});
+
+app.directive('polish', function () {
+    "use strict";
+
+    return {
+        require: 'greeting',
+        restrict: 'A',
+        link: function (scope, element, attrs, controller) {
+            controller.addGreeting('Cześć');
+        }
+    };
+});
+
 app.controller('GreetingController', ['$scope', function ($scope) {
     "use strict";
+    var greetings = ['Hello'];
     $scope.sayHello = function () {
-        alert('Hello');
+        alert(greetings.join());
     };
+    this.addGreeting = function (greeting) {
+        greetings.push(greeting);
+    };
+
 }]);
